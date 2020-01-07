331a161a8c38c40f36c6333d7dc9e7ada9c7fec0
Gary Clark <gclark04432@live.co.uk>
2020-01-07 21:50:00 +0000

    add-remove-dinosaur-from-park

--- models/park.js
+++ models/park.js
@@ -4,4 +4,12 @@ const Park = function(name, ticketPrice) {
   this.dinosaurs = [];
 }
 
+Park.prototype.addDinosaur = function (dinosaur) {
+  this.dinosaurs.push(dinosaur);
+};
+
+Park.prototype.removeDinosaur = function (dinosaur) {
+  this.dinosaurs.pop(dinosaur);
+};
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -5,9 +5,16 @@ const Dinosaur = require('../models/dinosaur.js');
 describe('Park', function() {
 
   let park;
+  let dinosaur;
+  let dinosaur_two;
+  let dinosaur_three;
 
   beforeEach(function () {
     park = new Park('Jurassic Park', 35);
+    dinosaur = new Dinosaur('t-rex', 'carnivore', 50);
+    dinosaur_two = new Dinosaur('diplodocus', 'herbivore', 35);
+    dinosaur_three = new Dinosaur('stegasaurus', 'herbivore', 30);
+
   })
 
   it('should have a name', function(){
@@ -25,9 +32,20 @@ describe('Park', function() {
     assert.deepStrictEqual(actual, []);
   });
 
-  it('should be able to add a dinosaur to its collection');
+  it('should be able to add a dinosaur to its collection', function() {
+    park.addDinosaur(dinosaur);
+    const actual = park.dinosaurs.length;
+    assert.strictEqual(actual, 1)
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to remove a dinosaur from its collection', function() {
+    park.addDinosaur(dinosaur);
+    park.addDinosaur(dinosaur_two);
+    park.addDinosaur(dinosaur_three);
+    park.removeDinosaur(dinosaur_three);
+    const actual = park.dinosaurs.length;
+    assert.strictEqual(actual, 2)
+  });
 
   it('should be able to find the dinosaur that attracts the most visitors');
 
